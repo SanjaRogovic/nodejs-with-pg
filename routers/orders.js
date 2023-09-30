42511807ea6177c28abd9a8ffddce7c278479c9c
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-30 12:27:08 +0200

    added GET method for all orders

--- routers/orders.js
+++ routers/orders.js
@@ -6,5 +6,32 @@ const {Pool} = pg
 
 const pool = new Pool()
 
+// ROUTE to ALL ORDERS - GET method
+
+ordersRouter.get("/", async (req,res) => {
+    try {
+        const result = await pool.query("SELECT * FROM orders;")
+        // console.log(result.rows)
+        res.json(result.rows)
+    }
+    catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+
+// ROUTE to ONE ORDER (with the id) - GET method
+
+ordersRouter.get("/:id", async (req, res) => {
+    const {id} = req.params
+    try {
+        const result = await pool.query("SELECT * FROM orders WHERE id=$1;", [id])
+        res.json(result.rows[0])
+    }
+    catch (error) {
+        res.status(404).json ("Order not found", error)
+    }
+})
+
 
 export default ordersRouter
