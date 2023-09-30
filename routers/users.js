5c0062a55c4a0152553d0c7dcdb301de9f9bd7d9
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-30 12:10:09 +0200

    added PUT and DELETE method

--- routers/users.js
+++ routers/users.js
@@ -35,24 +35,71 @@ usersRouter.get("/:id", async (req, res) => {
         res.json(result.rows[0])
     }
     catch (error) {
-        res.status(404).json (error)
+        res.status(404).json ("User not found", error)
     }
 })
 
 
-// ROUTE to create a NEW USER - POST method
+// ROUTE to CREATE a NEW USER - POST method
 
 usersRouter.post("/", async (req, res) => {
     const {first_name, last_name, age} = req.body;
-    console.log(req.body)
+    // console.log(req.body)
     try {
-        const result = await pool.query("INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *;", [first_name, last_name, age])
+        const result = await pool.query(
+            "INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *;", [first_name, last_name, age])
         res.json(result.rows[0])
     }
     catch (error) {
         res.status(500).json(error)
     }
-    
+})
+
+
+// ROUTE to EDIT one user - PUT method  - How to edit all for user? Below code not working
+
+// usersRouter.put("/:id", async (req, res) => {
+//     const {id} = req.params
+//     const {first_name, last_name, age} = req.body
+//     try {
+//         const result = await pool.query(
+//             "UPDATE users SET (first_name=$1, last_name=$2, age=$3) WHERE id=$4 RETURNING *;", [first_name, last_name, age, id])
+//         res.json(result.rows[0])
+//     }
+//     catch (error) {
+//         res.status(404).json("User not found", error)
+//     }
+
+// })
+
+
+usersRouter.put("/:id", async (req, res) => {
+    const {id} = req.params
+    const {first_name} = req.body
+    try {
+        const result = await pool.query(
+            "UPDATE users SET first_name=$1 WHERE id=$2 RETURNING *;", [first_name, id])
+        res.json(result.rows[0])
+    }
+    catch (error) {
+        res.status(404).json("User not found", error)
+    }
+
+})
+
+// Route to DELETE user (with the id)
+
+usersRouter.delete("/:id", async (req, res) => {
+    const {id} = req.params
+    try {
+        const result = await pool.query(
+            "DELETE FROM users WHERE id=$1 RETURNING *;", [id])
+        res.json(result.rows[0])
+    }
+    catch (error) {
+        res.status(404).json("User not found", error)
+    }
+
 })
 
   export default usersRouter;
