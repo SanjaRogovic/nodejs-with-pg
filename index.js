2185a07f0d494040facf17d0a756c293b792b446
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-30 11:10:28 +0200

    added get method to all users

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import usersRouter from "./routers/users.js"
 
 const app = express()
 
-// app.use(express.json())
+app.use(express.json())
+app.use("/api/users", usersRouter)
 
 
 const port = process.env.PORT || 8080
--- routers/users.js
+++ routers/users.js
@@ -5,9 +5,24 @@ const {Pool} = pg
 
 const pool = new Pool()
 
+// TESTING DATABASE CONNECTION
 //   pool.query("SELECT NOW()")
 //   .then(data => console.log("data retrieved", data.rows[0]))
-
+//   .catch(error => console.log("error retrieved", error))
 //   console.log(process.env.PGUSER)
+
+
+// ROUTE to ALL USERS
+
+usersRouter.get("/", async (req,res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users;")
+        // console.log(result.rows)
+        res.json(result.rows)
+    }
+    catch (error) {
+        res.status(404).json(error)
+    }
+})
 
   export default usersRouter;
