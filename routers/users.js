b368b604f5f8f34d779e750c29502a764ab31d24
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-30 11:31:41 +0200

    added POST method to create new user

--- routers/users.js
+++ routers/users.js
@@ -12,7 +12,7 @@ const pool = new Pool()
 //   console.log(process.env.PGUSER)
 
 
-// ROUTE to ALL USERS
+// ROUTE to ALL USERS - GET method
 
 usersRouter.get("/", async (req,res) => {
     try {
@@ -23,6 +23,36 @@ usersRouter.get("/", async (req,res) => {
     catch (error) {
         res.status(404).json(error)
     }
+})
+
+
+// ROUTE to ONE USER (with the id) - GET method
+
+usersRouter.get("/:id", async (req, res) => {
+    const {id} = req.params
+    try {
+        const result = await pool.query("SELECT * FROM users WHERE id=$1;", [id])
+        res.json(result.rows[0])
+    }
+    catch (error) {
+        res.status(404).json (error)
+    }
+})
+
+
+// ROUTE to create a NEW USER - POST method
+
+usersRouter.post("/", async (req, res) => {
+    const {first_name, last_name, age} = req.body;
+    console.log(req.body)
+    try {
+        const result = await pool.query("INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *;", [first_name, last_name, age])
+        res.json(result.rows[0])
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+    
 })
 
   export default usersRouter;
